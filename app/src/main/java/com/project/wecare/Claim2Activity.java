bbec1439d234a5557dbed8c09aa58dfadfcb26b9
Afra Hussaindeen <mhfafra@gmail.com>
2022-04-01 08:03:28 +0530

    Add claim part 2

---
+++ app/src/main/java/com/project/wecare/Claim2Activity.java
@@ -0,0 +1,23 @@
+package com.project.wecare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Claim2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claim2);
+    }
+
+    public void onClickBtn(View v) {
+        Intent intent = new Intent(this, ClaimActivity.class);
+        startActivity(intent);
+    }
+}
