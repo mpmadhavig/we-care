a0c3f62c35219f048cc7ca4f55a0fe4b3b8f8037
Afra Hussaindeen <mhfafra@gmail.com>
2022-04-06 06:55:13 +0530

    Remove login title bar

--- app/src/main/java/com/project/wecare/LoginActivity.java
+++ app/src/main/java/com/project/wecare/LoginActivity.java
@@ -39,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
         et_nic = (EditText) findViewById(R.id.txtLoginNIC);
